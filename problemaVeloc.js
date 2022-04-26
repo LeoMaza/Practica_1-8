c584db24086c0d6c6166e785a4247898297bb807
Leo Maza <leomaza2222@gmail.com>
2022-04-26 10:26:33 -0300

    solucion de problema matematico

---
+++ problemaVeloc.js
@@ -0,0 +1,7 @@
+// En este problema debemos calcular cuanto tiempo se demora un automovil en alcanzar 60 km/h si su velocidad es de 20 km/h
+
+var velocidadTotal=60;
+var aceleracion= 20;
+var tiempo= velocidadTotal/aceleracion;
+
+console.log("El tiempo que se demora en alcanzar 60 km/h es:"+ tiempo + " horas");
